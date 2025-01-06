ad2b5e90498c9dca5cb514bdc168f37252354c29
faizanalibaig <riazfiaz6181@gmail.com>
2025-01-06 13:28:26 +0500

    clear & number buttons functionality completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,21 @@ import React, { useState } from 'react';
 import Data from './data/application.json';
 
 const App = () => {
+  const [displayValue, setDisplayValue] = useState(3991);
+
+  const handleClearDisplayValue = () => {
+    setDisplayValue(0);
+  };
+
+  const handleNumberClick = (number) => {
+    console.log('Number clicked');
+    if (displayValue === 0) {
+      setDisplayValue(number);
+    } else {
+      setDisplayValue(`${displayValue}${number}`);
+    }
+  };
+
   return (
     <div
       style={{
@@ -15,18 +30,23 @@ const App = () => {
       }}
       className='font-lage'
     >
-      <CalculatorHeader />
-      <Display />
-      <Board />
+      <CalculatorHeader>
+        <ToggleButton themeNumber={[1, 2, 3]} />
+      </CalculatorHeader>
+      <Display value={displayValue} />
+      <Board
+        clear={handleClearDisplayValue}
+        displayNumber={handleNumberClick}
+      />
     </div>
   );
 };
 
-function CalculatorHeader() {
+function CalculatorHeader({ children }) {
   return (
     <div className='text-[#ffffff] font-bold text-[28px] w-[500px] flex justify-between items-center'>
       <h1>{Data.title}</h1>
-      <ToggleButton themeNumber={[1, 2, 3]} />
+      {children}
     </div>
   );
 }
@@ -42,13 +62,13 @@ function ToggleButton({ themeNumber }) {
     <section className='flex items-center gap-6 h-full relative font-semibold'>
       <h3 className='text-[15px]'>theme</h3>
       <div>
-        {/* <div className='flex justify-between px-2 absolute bottom-6 w-[50%]'>
+        <div className='flex justify-between px-2 absolute bottom-10 w-[50%]'>
           {themeNumber.map((value, index) => (
             <h1 key={index} className='text-[12px]'>
               {value}
             </h1>
           ))}
-        </div> */}
+        </div>
         <div className='bg-[#182034] px-1 py-[3px] rounded-2xl w-16 h-[22px] flex items-center justify-between'>
           {themeNumber.map((value, index) => (
             <span
@@ -65,40 +85,114 @@ function ToggleButton({ themeNumber }) {
   );
 }
 
-function Display() {
+function Display({ value }) {
   return (
     <div className='text-[hsl(0,0%,100%)] font-lage text-4xl font-bold w-[500px] bg-[#182034] h-24 rounded-lg px-6 flex items-center justify-end'>
-      <h1>399, 981</h1>
+      <h1>{value}</h1>
     </div>
   );
 }
 
-function Board() {
+const DataButtons = {
+  NumberedButtons: [7, 8, 9, 6, 5, 4, 3, 2, 1, '.', 0, '/'],
+  OperationButtons: ['C', '+', '-', 'x'],
+  AdditionalButtons: ['Reset', '='],
+};
+function Board({ clear, displayNumber }) {
   return (
     <div
       style={{
         backgroundColor: '#182034',
         marginTop: '20px',
-        height: '400px',
+        height: '420px',
         width: '500px',
         borderRadius: '8px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: '16px',
+        padding: '20px 0px',
       }}
     >
-      <div className='flex flex-wrap justify-evenly px-2 py-5'>
-        <Button value={7} />
-        <Button value={8} />
-        <Button value={9} />
-        <Button value='DEL' backgroundColor='#637097' color='#ffffff' />
+      <div className='flex justify-center w-full'>
+        <div className='grid grid-cols-3 gap-4 px-2'>
+          {DataButtons.NumberedButtons.map((value, index) => (
+            <Button
+              value={value}
+              key={index}
+              width='100px'
+              clear={clear}
+              displayNumber={displayNumber}
+            />
+          ))}
+        </div>
+        <div className='grid grid-cols-1 gap-4 px-2'>
+          {DataButtons.OperationButtons.map((value, index) => (
+            <Button
+              value={value}
+              key={index}
+              backgroundColor={index === 0 ? '#637097' : ''}
+              color={index === 0 ? '#ffffff' : ''}
+              width='100px'
+              clear={clear}
+              displayNumber={displayNumber}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className='grid grid-cols-2 gap-4 px-2'>
+        {DataButtons.AdditionalButtons.map((value, index) => (
+          <Button
+            value={value}
+            key={index}
+            backgroundColor={index === 0 ? '#637097' : '#d03f2f'}
+            color='#ffffff'
+            width='215px'
+            clear={clear}
+            displayNumber={displayNumber}
+          />
+        ))}
       </div>
     </div>
   );
 }
 
-function Button({ value, backgroundColor, color }) {
+function Button({
+  value,
+  backgroundColor,
+  color,
+  width,
+  clear,
+  displayNumber,
+}) {
+  // let result = [];
+
+  // if (typeof value === 'number') {
+  //   result.push(value);
+  // }
+
+  // result.forEach((value) => {
+  //   console.log(value);
+  // });
+
+  const handleDisplay = (value) => {
+    if (value === 'C') {
+      clear();
+    }
+
+    if (typeof value === 'number') {
+      displayNumber(value);
+      console.log('Number: ' + value);
+    }
+  };
+
   return (
     <button
-      style={{ backgroundColor: backgroundColor, color: color }}
-      className='flex items-center justify-center h-[60px] w-[100px] rounded-[6px] bg-[#eae3dc] font-lage font-bold text-[28px] border-b-[5px] border-opacity-30 border-black pt-2'
+      style={{ backgroundColor: backgroundColor, color: color, width: width }}
+      onClick={() => handleDisplay(value)}
+      className='flex items-center justify-center h-[60px] rounded-[6px] bg-[#eae3dc] font-lage font-bold text-[28px] border-b-[4px] border-opacity-20 border-black pt-2'
     >
       {value}
     </button>
